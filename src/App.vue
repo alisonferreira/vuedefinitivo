315b12d06bdaef55c24d512286a1334cccd6ceb9
alisonferreira <alison.kyrie@gmail.com>
2021-05-12 20:20:27 -0300

    CARROSEL

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,29 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
     <button class="btn btn-outline">neutral</button>
+    <div class="w-64 carousel rounded-box">
+      <div class="w-full carousel-item">
+        <img src="https://picsum.photos/id/500/256/144" class="w-full" />
+      </div>
+      <div class="w-full carousel-item">
+        <img src="https://picsum.photos/id/501/256/144" class="w-full" />
+      </div>
+      <div class="w-full carousel-item">
+        <img src="https://picsum.photos/id/502/256/144" class="w-full" />
+      </div>
+      <div class="w-full carousel-item">
+        <img src="https://picsum.photos/id/503/256/144" class="w-full" />
+      </div>
+      <div class="w-full carousel-item">
+        <img src="https://picsum.photos/id/504/256/144" class="w-full" />
+      </div>
+      <div class="w-full carousel-item">
+        <img src="https://picsum.photos/id/505/256/144" class="w-full" />
+      </div>
+      <div class="w-full carousel-item">
+        <img src="https://picsum.photos/id/506/256/144" class="w-full" />
+      </div>
+    </div>
   </div>
   <router-view />
 </template>
