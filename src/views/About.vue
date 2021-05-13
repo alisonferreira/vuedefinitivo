5354e4e861761619b9f148c72154c4a9d02e23b6
alisonferreira <alison.kyrie@gmail.com>
2021-05-13 10:05:48 -0300

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,63 +1,65 @@
 <template>
-  <div class="md:inline-flex">
-    <div class="about p-8">
-      <div class="flex w-auto space-x-10 flex-nowrap">
-        <div
-          class="
-            artboard
-            phone-1
-            artboard-demo
-            border border-transparent
-            shadow-2xl
-          "
-        >
-          <div class="card text-center">
-            <figure class="px-10 pt-10">
-              <img
-                src="https://picsum.photos/id/1005/400/250"
-                class="rounded-xl"
-              />
-            </figure>
-            <div class="card-body">
-              <h2 class="card-title">shadow, center, padding</h2>
-              <p>
-                Rerum reiciendis beatae tenetur excepturi aut pariatur est eos.
-                Sit sit necessitatibus veritatis sed molestiae voluptates
-                incidunt iure sapiente.
-              </p>
-              <div class="justify-center card-actions">
-                <button class="btn btn-outline btn-black">More info</button>
+  <div class="bg-white dark:bg-gray-800">
+    <div class="md:inline-flex">
+      <div class="about p-8">
+        <div class="flex w-auto space-x-10 flex-nowrap">
+          <div
+            class="
+              artboard
+              phone-1
+              artboard-demo
+              border border-transparent
+              shadow-2xl
+            "
+          >
+            <div class="card text-center">
+              <figure class="px-10 pt-10">
+                <img
+                  src="https://picsum.photos/id/1005/400/250"
+                  class="rounded-xl"
+                />
+              </figure>
+              <div class="card-body">
+                <h2 class="card-title">shadow, center, padding</h2>
+                <p>
+                  Rerum reiciendis beatae tenetur excepturi aut pariatur est
+                  eos. Sit sit necessitatibus veritatis sed molestiae voluptates
+                  incidunt iure sapiente.
+                </p>
+                <div class="justify-center card-actions">
+                  <button class="btn btn-outline btn-black">More info</button>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-      <div class="flex w-auto space-x-10 flex-nowrap">
-        <div
-          class="
-            artboard
-            phone-1
-            artboard-demo
-            border border-transparent
-            shadow-2xl
-          "
-        >
-          <div class="card text-center">
-            <figure class="px-10 pt-10">
-              <img
-                src="https://picsum.photos/id/1005/400/250"
-                class="rounded-xl"
-              />
-            </figure>
-            <div class="card-body">
-              <h2 class="card-title">shadow, center, padding</h2>
-              <p>
-                Rerum reiciendis beatae tenetur excepturi aut pariatur est eos.
-                Sit sit necessitatibus veritatis sed molestiae voluptates
-                incidunt iure sapiente.
-              </p>
-              <div class="justify-center card-actions">
-                <button class="btn btn-outline btn-black">More info</button>
+        <div class="flex w-auto space-x-10 flex-nowrap">
+          <div
+            class="
+              artboard
+              phone-1
+              artboard-demo
+              border border-transparent
+              shadow-2xl
+            "
+          >
+            <div class="card text-center">
+              <figure class="px-10 pt-10">
+                <img
+                  src="https://picsum.photos/id/1005/400/250"
+                  class="rounded-xl"
+                />
+              </figure>
+              <div class="card-body">
+                <h2 class="card-title">shadow, center, padding</h2>
+                <p>
+                  Rerum reiciendis beatae tenetur excepturi aut pariatur est
+                  eos. Sit sit necessitatibus veritatis sed molestiae voluptates
+                  incidunt iure sapiente.
+                </p>
+                <div class="justify-center card-actions">
+                  <button class="btn btn-outline btn-black">More info</button>
+                </div>
               </div>
             </div>
           </div>
