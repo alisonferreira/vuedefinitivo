892e6d8ed5f2faf3537d443e10963fb512888515
alisonferreira <alison.kyrie@gmail.com>
2021-05-13 11:06:05 -0300

    d

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,4 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+  <div id="nav"></div>
   <router-view />
 </template>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,69 +1,26 @@
 <template>
-  <div class="bg-white dark:bg-gray-800">
-    <div class="md:inline-flex">
-      <div class="about p-8">
-        <div class="flex w-auto space-x-10 flex-nowrap">
-          <div
-            class="
-              artboard
-              phone-1
-              artboard-demo
-              border border-transparent
-              shadow-2xl
-            "
-          >
-            <div class="card text-center">
-              <figure class="px-10 pt-10">
-                <img
-                  src="https://picsum.photos/id/1005/400/250"
-                  class="rounded-xl"
-                />
-              </figure>
-              <div class="card-body">
-                <h2 class="card-title">shadow, center, padding</h2>
-                <p>
-                  Rerum reiciendis beatae tenetur excepturi aut pariatur est
-                  eos. Sit sit necessitatibus veritatis sed molestiae voluptates
-                  incidunt iure sapiente.
-                </p>
-                <div class="justify-center card-actions">
-                  <button class="btn btn-outline btn-black">More info</button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="flex w-auto space-x-10 flex-nowrap">
-          <div
-            class="
-              artboard
-              phone-1
-              artboard-demo
-              border border-transparent
-              shadow-2xl
-            "
-          >
-            <div class="card text-center">
-              <figure class="px-10 pt-10">
-                <img
-                  src="https://picsum.photos/id/1005/400/250"
-                  class="rounded-xl"
-                />
-              </figure>
-              <div class="card-body">
-                <h2 class="card-title">shadow, center, padding</h2>
-                <p>
-                  Rerum reiciendis beatae tenetur excepturi aut pariatur est
-                  eos. Sit sit necessitatibus veritatis sed molestiae voluptates
-                  incidunt iure sapiente.
-                </p>
-                <div class="justify-center card-actions">
-                  <button class="btn btn-outline btn-black">More info</button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+  <div class="md:inline-flex w-auto space-x-10 flex-nowrap p-6">
+    <div class="w-full space-x-4 carousel carousel-center bg-white rounded-box">
+      <div class="carousel-item">
+        <img src="https://picsum.photos/id/500/256/144" class="rounded-box" />
+      </div>
+      <div class="carousel-item">
+        <img src="https://picsum.photos/id/501/256/144" class="rounded-box" />
+      </div>
+      <div class="carousel-item">
+        <img src="https://picsum.photos/id/502/256/144" class="rounded-box" />
+      </div>
+      <div class="carousel-item">
+        <img src="https://picsum.photos/id/503/256/144" class="rounded-box" />
+      </div>
+      <div class="carousel-item">
+        <img src="https://picsum.photos/id/504/256/144" class="rounded-box" />
+      </div>
+      <div class="carousel-item">
+        <img src="https://picsum.photos/id/505/256/144" class="rounded-box" />
+      </div>
+      <div class="carousel-item">
+        <img src="https://picsum.photos/id/506/256/144" class="rounded-box" />
       </div>
     </div>
   </div>
